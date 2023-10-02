f3a1fb31ca83947e91770fdd4655fb7e2ca45b1f
sdlvlamp3 <140354850+sdlvlamp3@users.noreply.github.com>
2023-10-01 22:21:13 -0500

    commit27
    project created, header established

--- angular/project/project-app/src/app/app.module.ts
+++ angular/project/project-app/src/app/app.module.ts
@@ -2,25 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import  {HeaderComponent} from './header.component/header.component';
+import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
+    ShoppingListComponent,
     RecipeListComponent,
-    RecipeDetailComponent,
-
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
   ],
-
   imports: [
     BrowserModule,
-    FormsModule,
-    HeaderComponent,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
