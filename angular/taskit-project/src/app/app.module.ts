6a6cb72b2daed09ca073d53494d07e992ee272ed
sdlvlamp3 <140354850+sdlvlamp3@users.noreply.github.com>
2023-10-04 19:30:12 -0500

    commit31

--- angular/taskit-project/src/app/app.module.ts
+++ angular/taskit-project/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LeftMenuComponent } from './left-menu/left-menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LeftMenuComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
